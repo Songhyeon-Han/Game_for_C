b131adf72a0a348aeddbf0ac2f7e47d7763647a0
한송현 <yades2438@gmail.com>
2026-09-04 08:51:37 +0900

    create for bricks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <vector>
 
 const int WIDTH = 40;    // 게임 창 가로 한계 
 const int HEIGHT = 20;   // 게임 창 세로 한계
@@ -9,6 +10,37 @@ void setCursor(int x, int y) {
 	COORD pos = { (SHORT)x, (SHORT)y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+
+class Brick {
+private:
+	int x;
+	int y;
+	bool alive;    // true = 아직 안 깨짐
+
+public:
+	Brick(int startX, int startY) {
+		x = startX;
+		y = startY;
+		alive = true; // 벽돌은 태어날 때 당연히 멀쩡한 상탱
+	}
+
+	void draw() {
+		if (alive) {
+			setCursor(x, y);
+			std::cout << "#";
+		}
+	}
+
+	int getX () const { return x; } // 값을 읽기만 하는 창구
+	int getY() const { return y; }  // 값을 읽기만 하는 창구
+	bool isAlive() const { return alive; } // 값을 읽기만 하는 창구
+
+	void destroy() {          // 깨졌을 때 호출
+		alive = false;
+		setCursor(x, y);
+		std::cout << " ";    // 화면에서도 지운다
+	}
+};
 
 class Ball{
 private:
@@ -42,7 +74,7 @@ public:
 
 		if (y >= HEIGHT - 2) {                            // 패들 줄 바로 위에 도달했다면
 			if (x >= paddleX - 1 && x <= paddleX + 1) {   // &&는 두 값이 참일때. paddleX가 가로로 ±1칸 안 (3칸 안)
-				dy = -dy;
+				dy = -dy; // dy는 높이고 공이 튕긴다
 			}
 			else {
 				return false;
@@ -101,6 +133,13 @@ int main() {
 	Ball ball ( 20, 5, 1, 1 );            // 기존에 {}을 넣어서 값을 넣었고, 생성자가 생기면서 ()로 바뀜. = 도 사라짐
 	                                      // x,y는 서로 붙어 있어야 할 필요 없고, x는 가로 몇번째  y는 세로 몇번째로 서로 독립적
 
+	std::vector<Brick> bricks;
+	for (int row = 1; row <= 3; row++) {
+		for (int col = 5; col < WIDTH - 5; col++) {
+			bricks.push_back(Brick(col, row));
+		}
+	}
+
 	while (true) {
 		
 		paddle.update();
@@ -111,6 +150,10 @@ int main() {
 		}
 		paddle.draw();
 		ball.draw();    // 그리기도 이 한줄
+
+		for (Brick& brick : bricks) {
+			brick.draw();
+		}
 
 		Sleep(33);
 		
